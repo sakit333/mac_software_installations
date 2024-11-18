8f5fdd1b536b83f79fb43414ed151dc4464445a0
SAK <120071904+sakit333@users.noreply.github.com>
2024-11-18 17:37:40 +0530

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh &&
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile &&
 eval "$(/opt/homebrew/bin/brew shellenv)"
